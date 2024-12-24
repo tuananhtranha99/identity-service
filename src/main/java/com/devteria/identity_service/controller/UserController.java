2e9c20606cb49750cadf5ad294a17bdf6ba38b88
anhtht <quyetthang@vnpt.vn>
2024-12-24 17:05:56 +0700

    first commit

---
+++ src/main/java/com/devteria/identity_service/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.devteria.identity_service.controller;
+
+import com.devteria.identity_service.dto.request.ApiResponse;
+import com.devteria.identity_service.dto.request.UserCreationRequest;
+import com.devteria.identity_service.dto.request.UserUpdateRequest;
+import com.devteria.identity_service.entity.User;
+import com.devteria.identity_service.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    ApiResponse<User> createUser(@RequestBody @Valid UserCreationRequest request){
+        ApiResponse<User> apiResponse = new ApiResponse<>();
+        apiResponse.setResult(userService.createUser(request));
+
+        return apiResponse;
+    }
+
+    @GetMapping
+    List<User> getUsers(){
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUsers(@PathVariable("userId") String userId){
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/{userId}")
+    User updateUser(@PathVariable("userId") String userId, @RequestBody UserUpdateRequest request){
+        return userService.updateUser(userId, request);
+    }
+
+    @DeleteMapping("/{userId}")
+    String deleteUser(@PathVariable("userId") String userId){
+        userService.deleteUser(userId);
+        return "User has been deleted";
+    }
+}
---
+++ src/main/java/com/devteria/identity_service/service/UserService.java
@@ -0,0 +1,56 @@
+package com.devteria.identity_service.service;
+
+import com.devteria.identity_service.dto.request.UserCreationRequest;
+import com.devteria.identity_service.dto.request.UserUpdateRequest;
+import com.devteria.identity_service.entity.User;
+import com.devteria.identity_service.exception.AppException;
+import com.devteria.identity_service.exception.ErrorCode;
+import com.devteria.identity_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserCreationRequest request){
+        User user = new User();
+
+        if (userRepository.existsByUsername(request.getUsername()))
+            throw new AppException(ErrorCode.USER_EXISTED);
+
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id){
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User updateUser(String userId, UserUpdateRequest request){
+        User user = getUser(userId);
+
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String userId){
+        userRepository.deleteById(userId);
+    }
+}
