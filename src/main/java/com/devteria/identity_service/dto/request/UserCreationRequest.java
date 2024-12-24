3db5e5e18ba343f2168a7333a49649b0bbe1feae
anhtht <anhtht@runsystem.net>
2024-12-24 20:06:10 +0700

    .

--- src/main/java/com/devteria/identity_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/devteria/identity_service/dto/request/UserCreationRequest.java
@@ -1,57 +1,18 @@
 package com.devteria.identity_service.dto.request;
 
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
 import java.time.LocalDate;
 
+@Data
 public class UserCreationRequest {
-    @Size(min = 3, message = "Username must be at least 3 characters")
+    @Size(min = 3, message = "USERNAME_INVALID")
     private String username;
 
-    @Size(min = 8, message = "Password must be at least 8 characters")
-    @Size(max = 9, message = "Password must be a maximum of 9 characters")
+    @Size(min = 8, message = "INVALID_PASSWORD")
     private String password;
     private String firstName;
     private String lastName;
     private LocalDate dob;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
 }
